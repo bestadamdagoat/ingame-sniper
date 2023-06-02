4e4cea48addf5bfcf82498e005de83b95a5e0687
Adam O <66372881+bestadamdagoat@users.noreply.github.com>
2023-06-01 21:48:09 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,6 @@
 getgenv().assetid = Text
+getgenv().placeid = Text
+getgenv().buyitemafterteleport = false
 
 local response = request({
 	Url = "https://raw.githubusercontent.com/bestadamdagoat/ingame-sniper/main/VERSION.md",
@@ -36,7 +38,7 @@ local Tab = Window:CreateTab("Main") -- Title, Image
 local Section = Tab:CreateSection("Prompt Purchase (Sniper)")
 local Input = Tab:CreateInput({
     Name = "Asset ID",
-    PlaceholderText = "13023433259",
+    PlaceholderText = "ex: 13023433259",
     RemoveTextAfterFocusLost = false,
     Callback = function(Text)
         getgenv().assetid = Text
@@ -60,5 +62,63 @@ local Toggle = Tab:CreateToggle({
     end,
 })
 
+local Section = Tab:CreateSection("Teleport")
+local Input = Tab:CreateInput({
+    Name = "Place ID",
+    PlaceholderText = "ex: 263761432",
+    RemoveTextAfterFocusLost = false,
+    Callback = function(Text)
+        getgenv().placeid = Text
+    end,
+})
+local Input = Tab:CreateInput({
+    Name = "Asset ID (reqs toggle to be on)",
+    PlaceholderText = "ex: 13023433259",
+    RemoveTextAfterFocusLost = false,
+    Callback = function(Text)
+        writefile("afterassetid.txt", Text)
+    end,
+})
+local Toggle = Tab:CreateToggle({
+    Name = "Buy an Item After Teleporting?",
+    CurrentValue = false,
+    Flag = "TPafterPurchase", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function(Value)
+        getgenv().buyitemafterteleport = Value
+    end,
+})
+local Button = Tab:CreateButton({
+    Name = "Teleport to Game",
+    Callback = function()
+        if buyitemafterteleport == true then
+            queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/bestadamdagoat/ingame-sniper/main/buyafterteleport.lua'))()")
+            game:GetService("TeleportService"):Teleport(placeid, game:GetService("Players").LocalPlayer)
+        else
+            game:GetService("TeleportService"):Teleport(placeid, game:GetService("Players").LocalPlayer)
+        end
+    end,
+})
+
+local Tab = Window:CreateTab("Scripts") -- Title, Image
+local Button = Tab:CreateButton({
+    Name = "Secured Dark Dex v3 (Explorer)",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua", true))()
+    end,
+})
+local Button = Tab:CreateButton({
+    Name = "Infinite Yield (Admin)",
+    Callback = function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+    end,
+})
+local Button = Tab:CreateButton({
+    Name = "Simple Spy (Remotes)",
+    Callback = function()
+        loadstring(game:HttpGet("https://github.com/exxtremestuffs/SimpleSpySource/raw/master/SimpleSpy.lua"))()
+    end,
+})
+
 local Tab = Window:CreateTab("About") -- Title, Image
-local Paragraph = Tab:CreateParagraph({Title = "Version 0.5", Content = "Latest Version is " .. response.Body .. " Make sure to launch using the script provided in the README. If you don't, you'll be running an outdated version! bestadamdagoat/ingame-sniper"})
+local Section = Tab:CreateSection("Info")
+local Paragraph = Tab:CreateParagraph({Title = "Version 1.0", Content = "Latest Version is " .. response.Body .. " Make sure to launch using the script provided in the README. If you don't, you'll be running an outdated version! bestadamdagoat/ingame-sniper"})
